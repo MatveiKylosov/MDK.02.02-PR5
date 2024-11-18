bd9fd042ffab5b5e028ff9d34d8679d16c715b0d
matvei <matveikylosov@gmail.com>
2024-11-18 20:26:56 +0500

    Был написан TCPServer и main

--- SocketServer/Program.cs
+++ SocketServer/Program.cs
@@ -2,14 +2,123 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SocketServer
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static private void PrintLogo()
         {
+            Console.WriteLine(@"           /$$                 /$$                                 ");
+            Console.WriteLine(@"          | $$                | $$                                 ");
+            Console.WriteLine(@"  /$$$$$$ | $$$$$$$   /$$$$$$ | $$$$$$$   /$$$$$$                  ");
+            Console.WriteLine(@" |____  $$| $$__  $$ /$$__  $$| $$__  $$ |____  $$                 ");
+            Console.WriteLine(@"  /$$$$$$$| $$  \ $$| $$  \ $$| $$  \ $$  /$$$$$$$                 ");
+            Console.WriteLine(@" /$$__  $$| $$  | $$| $$  | $$| $$  | $$ /$$__  $$                 ");
+            Console.WriteLine(@"|  $$$$$$$| $$$$$$$/|  $$$$$$/| $$$$$$$/|  $$$$$$$                 ");
+            Console.WriteLine(@" \_______/|_______/  \______/ |_______/  \_______/                 ");
+            Console.WriteLine(@"                                                                   ");
+            Console.WriteLine(@"                                                                   ");
+            Console.WriteLine(@"                                                                   ");
+            Console.WriteLine(@"                                           /$$                     ");
+            Console.WriteLine(@"                                          | $$                     ");
+            Console.WriteLine(@"                                      /$$$$$$$  /$$$$$$  /$$    /$$");
+            Console.WriteLine(@"                                     /$$__  $$ /$$__  $$|  $$  /$$/");
+            Console.WriteLine(@"                                    | $$  | $$| $$$$$$$$ \  $$/$$/ ");
+            Console.WriteLine(@"                                    | $$  | $$| $$_____/  \  $$$/  ");
+            Console.WriteLine(@"                                    |  $$$$$$$|  $$$$$$$   \  $/   ");
+            Console.WriteLine(@"                                     \_______/ \_______/    \_/    ");
+        }
+
+        /*
+         Func<string, T> parse
+         Первый параметр отвечает за то, что будет параметром в делегате (называется он in)
+         Второй, что получается на выходе (называется он TResult)
+         */
+
+        static T GetInput<T>(string prompt, Func<string, T> parse, Func<T, bool> validate)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                var input = Console.ReadLine();
+                var parsedValue = parse(input);
+                if (validate(parsedValue))
+                {
+                    return parsedValue;
+                }
+            }
+        }
+        static void Main()
+        {
+            PrintLogo();
+#if false
+            var regex = new Regex(@"^(\d{1,3}\.){3}\d{1,3}$");
+
+            var ip = GetInput("Укажите IP адрес сервера - ", 
+                               s => s, 
+                               s => regex.IsMatch(s));
+
+            var port = GetInput("Укажите порт сервера - ", 
+                                s => int.TryParse(s, out int p) ? p : -1, 
+                                p => p > 1025 && p < 65536);
+
+            var quantityClients = GetInput("Укажите максимальное количество клиентов - ",
+                                            s => int.TryParse(s, out int q) ? q : -1,
+                                            q => q > 0);
+
+            var tokenLifetime = GetInput("Укажите сколько будет активен токен в секундах (60 или более) - ", 
+                                          s => int.TryParse(s, out int t) ? t : -1, 
+                                          t => t > 60);
+
+
+            Console.WriteLine($"Итоговые настройки:");
+            Console.WriteLine($"IP адрес: {ip}");
+            Console.WriteLine($"Порт: {port}");
+            Console.WriteLine($"Максимальное количество клиентов: {quantityClients}");
+            Console.WriteLine($"Время жизни токена: {tokenLifetime} секунд");
+            Console.WriteLine($"\nдля просмотра всех команд используйте команду /help");
+#endif
+
+            while (true)
+            {
+                Console.Write(">>> ");
+                var command = Console.ReadLine();
+                 
+                if(command == "/help")
+                {
+                    Console.WriteLine($"Список доступных комманд:");
+                    Console.WriteLine($"/clear\t\tотчищает консоль.");
+                    Console.WriteLine($"/config\t\tобновить настройки сервера.");
+                    Console.WriteLine($"/status\t\tсписок подключённых клиентов к серверу.");
+                    Console.WriteLine($"/disconnect\tотключить клиента от сервера.");
+                    Console.WriteLine($"/help\t\tпросмотр всех команд.");
+                }
+
+                else if(command == "/clear")
+                {
+                    PrintLogo();
+                    Console.Clear();
+                }
+
+                else if (command == "/config")
+                {
+
+                }
+
+                else if (command == "/status")
+                {
+
+                }
+
+                else if (command == "/disconnect")
+                {
+
+                }
+            }
         }
     }
 }
---
+++ SocketServer/TCPServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    public class TCPServer
+    {
+        private readonly TcpListener TCPListener;
+        private bool IsRunning;
+        private readonly ConcurrentBag<TcpClient> Clients = new ConcurrentBag<TcpClient>();
+        private int QuntityClients;
+        private int TokenLifetime;
+
+        public TCPServer(string ip = "", int port = 1337, int quantityClients = 1, int tokenLifetime = 60)
+        {
+            var ipServer = ip == "" ? IPAddress.Any : IPAddress.Parse(ip);
+            TCPListener = new TcpListener(ipServer, port);
+            IsRunning = false;
+            QuntityClients = quantityClients;
+            TokenLifetime = tokenLifetime;
+        }
+
+        public async Task<bool> InitServer()
+        {
+            try
+            {
+                TCPListener.Start();
+                IsRunning = true;
+                Console.WriteLine("Сервер запущен...");
+
+                // Основной цикл прослушивания
+                while (IsRunning)
+                {
+                    if(Clients.Count() == QuntityClients)
+                    {
+                        //Давать отворот клиентам
+                        continue;
+                    }    
+
+                    try
+                    {
+                        var client = await TCPListener.AcceptTcpClientAsync();
+                        Clients.Add(client); // Добавляем клиента в список
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка подключения клиента: {ex.Message}");
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка запуска сервера: {ex.Message}");
+                return false;
+            }
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+            TCPListener.Stop();
+
+            // Закрываем все подключения
+            foreach (var client in Clients)
+            {
+                client.Close();
+            }
+        }
+    }
+}
